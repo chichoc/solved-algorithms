62fb1aa89b124c602c6b50f112edd4ecfb9ed853
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-01 11:30:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 6.35 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,8 +1,3 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    for (let i = 0; i < arr1.length; i++){
-        const sum = arr1[i].map((num, idx) => num + arr2[i][idx])
-        answer[i] = sum;
-    }
-    return answer;
+    return arr1.map((a, i) => a.map((b, j) => b + arr2[i][j]));
 }
