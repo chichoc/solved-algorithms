97ed3a9bac586f78d3e227d75185e0c95b98f1b0
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-01 11:33:39 +0900

    [level 1] Title: 직사각형 별찍기, Time: 58.54 ms, Memory: 29.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,8 +2,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
+    const row = '*'.repeat(a)
     
-    for (let h=0; h < b; h++) {
-        console.log('*'.repeat(a))
+    for (let h = 0; h < b; h++) {
+        console.log(row)
     }
 });
