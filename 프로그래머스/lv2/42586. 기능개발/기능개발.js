1810ad033fa175f52cd63ca66167368f1ec86dd8
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-05 17:49:10 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,15 +1,15 @@
 function solution(progresses, speeds) {
     const days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let standardDay = 0;
-    let count = 0;
-    const answer = [];
-    days.forEach((day, index) => {
-        if (standardDay < day) {
-            if (count > 0) answer.push(count);
+    let standardDay = days[0];
+    const answer = [0];
+    let answerIndex = 0;
+    
+    days.forEach((day) => {
+        if (day <= standardDay) answer[answerIndex] ++;
+        else {
             standardDay = day;
-            count = 1;
-        } else count++;
-        if (index === days.length - 1) answer.push(count);
+            answer[++answerIndex] = 1;
+        }
     });
     return answer;
 }
