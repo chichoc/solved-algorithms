43b56825bedabcedaac87a990259d0b65ab3dfb3
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-10 15:15:52 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 30.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,4 +1,6 @@
 function solution(n) {
     if (n < 2) return n;
-    return [...Array(n - 2).keys()].reduce((prev, curr) => n % (curr + 2) === 0 ? prev + curr + 2 : prev, (1 + n));
+    const squareRoot = Number.isInteger(Math.sqrt(n)) ? Math.sqrt(n) : 0;
+    const loopCount = squareRoot ? Math.sqrt(n) - 2 : parseInt(Math.sqrt(n)) - 1;
+    return [...Array(loopCount).keys()].reduce((prev, curr) => n % (curr + 2) === 0 ? prev + (curr + 2) + (n / (curr + 2)) : prev, (1 + n + squareRoot));
 }
