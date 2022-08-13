4561bf3a9a232da2731479949cc83517600e1c4c
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-13 11:32:06 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,3 +1,3 @@
 function solution(s) {
-    return [4, 6].some((l) => l === s.length) && [...s].every((num) => !isNaN(num));
+    return [4, 6].some((l) => l === s.length) && ![...s].some(isNaN);
 }
