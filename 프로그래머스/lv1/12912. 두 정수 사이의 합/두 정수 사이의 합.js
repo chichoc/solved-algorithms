e00b9c18843cc22a836ad67028205c6a81757704
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-16 11:19:49 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.26 ms, Memory: 30.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,3 +1,5 @@
 function solution(a, b) {
-    return (a + b) * (Math.abs(b - a) + 1) / 2;
-}
+    let answer = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) answer += i
+    return answer;
+} 
