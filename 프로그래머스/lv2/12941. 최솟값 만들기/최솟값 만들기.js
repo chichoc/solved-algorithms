74d4d1733dbb8575755947b1d3d243b74a5f99cd
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-20 12:03:28 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.85 ms, Memory: 29.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,5 @@
 function solution(A, B){
-    let answer = 0;
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
-    A.forEach((n, i) => answer += n * B[i])
-    return answer;
+    return A.reduce((sum, num, i) => sum + num * B[i], 0);
 }
