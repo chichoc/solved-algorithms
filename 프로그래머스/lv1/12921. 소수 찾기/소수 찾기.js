241c759d32f3bde934d5d6f3b77c0cf1c90b17c4
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-21 20:57:53 +0900

    [level 1] Title: 소수 찾기, Time: 48.58 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let answer = 0;
     const num = Array(n - 1).fill(true);
-    for (let i = 2; i < n + 1; i++){
+    for (let i = 2; i <= Math.sqrt(n); i++){
         if (!num[i - 2]) continue;
         for (let j = i * i; j < n + 1; j += i){
             if (!num[j - 2]) continue;
