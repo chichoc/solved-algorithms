101abbb405c3e3ac7141793599bb9a00cff41a02
chichoc <61962901+chichoc@users.noreply.github.com>
2022-08-30 14:45:25 +0900

    [level 2] Title: 가장 큰 수, Time: 0.21 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,14 @@
 function solution(numbers) {
-    const answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join('');
+    const buckets = [];
+    for (const n of numbers){
+        const s = String(n), l = s.length;
+        let digit = '';
+        for (let i = 0; i < 4; i++){
+            digit += s[l > i ? i : i % l];
+        }
+        digit = 9999 - digit;
+        buckets[digit] = buckets[digit] ? buckets[digit] + s : s;
+    }
+    const answer = buckets.join``;
     return answer[0] === '0' ? '0' : answer;
 }
