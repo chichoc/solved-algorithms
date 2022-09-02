ea60bf218143d383a3af6cebf639cd9cf58e7cfc
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-02 23:54:17 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,15 +1,11 @@
 function solution(new_id) {
-    if (!new_id.length) return 'a';
-    const strToRemove = /[^0-9a-z-_.]/g;
-    const consecutivePeriod = /[.]{2,}/g;
-    let idToSuggest = new_id
-        .toLowerCase()
-        .replace(strToRemove, '')
-        .replace(consecutivePeriod, '.')
-        .replace(/[.]$/, '')
-        .replace(/^[.]/, '');
-    if (idToSuggest.length >= 16) return idToSuggest.slice(0, 15).replace(/[.]$/, '');
-    if (!idToSuggest.length) idToSuggest = 'a';
-    if (idToSuggest.length <= 2) return idToSuggest + idToSuggest.slice(-1).repeat(3 - idToSuggest.length);
-    return idToSuggest
+    const idToSuggest = new_id
+        .toLowerCase() // 1
+        .replace(/[^\w-_.]/g, '') // 2
+        .replace(/\.+/g, '.') // 3
+        .replace(/^\.|\.$/g, '') // 4
+        .replace(/^$/, 'a') // 5
+        .slice(0, 15).replace(/\.$/, ''); // 6
+    const length = idToSuggest.length;
+    return length > 2 ? idToSuggest : idToSuggest + idToSuggest.slice(-1).repeat(3 - length);
 }
