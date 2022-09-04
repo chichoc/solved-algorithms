b48089fdb6249901cbd4f7539b2c09e4c80431e6
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-04 22:34:11 +0900

    [level 1] Title: 실패율, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,12 +1,9 @@
 function solution(N, stages) {
-    let failureCount = Array(N + 1).fill(0);
-    stages.forEach(userStage => failureCount[--userStage]++);
-    
-    let failureRate = [...Array(N).keys()].map((e, i) => [i + 1, 0]);
-    failureRate.reduce((challengedUsers, element, index)=> {
-        element[1] = failureCount[index] / challengedUsers;
-        return challengedUsers - failureCount[index];
-    }, stages.length);
-    
-    return failureRate.sort((a, b) => b[1] - a[1]).map(e => e[0]);
+    let failureRate = [];
+    for (let i = 1; i <= N; i++){
+        let reach = stages.filter(s => s >= i).length;
+        let curr = stages.filter(s => s === i).length;
+        failureRate.push([i, curr / reach]);
+    }
+    return failureRate.sort((a, b) => b[1] - a[1]).map(a => a[0]);
 }
