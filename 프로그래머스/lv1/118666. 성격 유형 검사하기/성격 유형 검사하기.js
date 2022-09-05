62d5221df9264d5fc9d3c1b17a5869e1f7869594
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-05 23:30:51 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,16 +1,13 @@
 function solution(survey, choices) {
-    const type = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']]; 
+    const types = ['RT', 'CF', 'JM', 'AN']; 
     const scoreList = {};
-    type.forEach(([t1, t2]) => {
-        scoreList[t1] = 0;
-        scoreList[t2] = 0;
-    });
-    survey.forEach((type, index) => {
+    types.forEach(type => type.split('').forEach(char => scoreList[char] = 0));
+                 
+    survey.forEach(([disagree, agree], index) => {
         const choiceScore = choices[index];
         if (choiceScore === 4) return;
-        typeToAddScore = choiceScore < 4 ? type[0] : type[1];
-        scoreList[typeToAddScore] += Math.abs(4 - choiceScore);
+        scoreList[choiceScore < 4 ? disagree : agree] += Math.abs(4 - choiceScore);
     });
-    type.forEach(compareType => compareType.sort((t1, t2) => scoreList[t2] - scoreList[t1]));
-    return type.map(types => types[0]).join('');
+    
+    return types.map(([t1, t2]) => scoreList[t2] > scoreList[t1] ? t2 : t1).join('');
 }
