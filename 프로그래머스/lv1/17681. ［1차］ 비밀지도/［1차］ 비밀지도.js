8112c5ff3a92d646aaa1ecfaa102eff9915b0e8c
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-09 22:45:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,16 +1,3 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    return arr1.map((row, index) => {
-        // 2진수로 변환
-        const [changeRowOfArr1, changeRowOfArr2] = [row, arr2[index]].map(rowOfArray => {
-            const changeRow = ChangeBinaryNum(rowOfArray);
-            return '0'.repeat(n - changeRow.length) + changeRow;
-        });
-        // 1 = #, 0 = ' '로 변환
-        return [...changeRowOfArr1].map((num, index) => [num, changeRowOfArr2[index]].some(n => +n === 1) ? '#' : ' ').join('');
-    });
-}
-
-function ChangeBinaryNum(num) {
-   return num ? ChangeBinaryNum(parseInt(num / 2)) + num % 2 : '';
+    return arr1.map((row, index) => (row | arr2[index]).toString(2).padStart(n, 0).replace(/1|0/g, n => +n ? '#' : ' '));
 }
