38d0ed682aa5f77cad2e7481fd2443884b5a1255
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-11 00:14:28 +0900

    [level 2] Title: [1차] 캐시, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,17 +1,18 @@
 function solution(cacheSize, cities) {
     if (!cacheSize) return cities.length * 5;
-    const cache = [cities[0].toLowerCase()];
-    return cities.slice(1).reduce((time, city) => {
+    const cache = [];
+    let time = 0;
+    cities.forEach(city => {
         const lowerCity = city.toLowerCase();
         const hitIndex = cache.indexOf(lowerCity);
         if (hitIndex !== -1) {
-            cache.length === cacheSize && cache.splice(hitIndex, 1);
-            cache.push(lowerCity);
-            return time += 1;
+            cache.splice(hitIndex, 1);
+            time ++;
         } else {
             cache.length === cacheSize && cache.shift();
-            cache.push(lowerCity);
-            return time += 5;
+            time += 5;
         }
-    }, 5);
+        cache.push(lowerCity);
+    });
+    return time;
 }
