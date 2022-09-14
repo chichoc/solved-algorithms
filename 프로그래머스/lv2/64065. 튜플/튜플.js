d28b39768d55257128f408f93b9b9de7a4529b0f
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-15 01:15:46 +0900

    [level 2] Title: 튜플, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,4 +1,6 @@
 function solution(s) {
-    const sToArray = s.replace(/^{|}$/g, '').split('},').map(str => str.replace(/^{|}$/g, '').split(',')).sort((a, b) => a.length - b.length);
+    const sToArray = s.slice(2, -2).split('},{')
+                        .map(str => str.split(','))
+                        .sort((a, b) => a.length - b.length);
     return sToArray.reduce((tup, arr) => [...tup, arr.find(elem => !tup.includes(elem))], []).map(x => +x);
 }
