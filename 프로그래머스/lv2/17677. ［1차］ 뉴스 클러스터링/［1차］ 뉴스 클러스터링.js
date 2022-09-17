95085290839f3eb49d9ca22ba09b4055a81b4741
chichoc <61962901+chichoc@users.noreply.github.com>
2022-09-17 20:17:36 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -2,9 +2,8 @@ function solution(str1, str2) {
     let answer = 0;
     const str1ToArray = strToArray(str1, str1.length);
     const str2ToArray = strToArray(str2, str2.length);
-    const str1ToArrayLen = str1ToArray.length;
-    const str2ToArrayLen = str2ToArray.length;
-    if (!str1ToArrayLen && !str2ToArrayLen) return 65536;
+    let union = str1ToArray.length + str2ToArray.length;
+    if (!union) return 65536;
     
     let interSection = 0;
     str1ToArray.forEach(str => {
@@ -14,7 +13,8 @@ function solution(str1, str2) {
             interSection++;
         }
     });
-    answer = interSection / (str1ToArrayLen + str2ToArrayLen - interSection);
+    union -= interSection;
+    answer = interSection / union;
     return Math.floor(answer * 65536);
 }
 
@@ -22,7 +22,7 @@ function strToArray(str) {
     const array = [];
      for (let i = 0; i < str.length - 1; i++) {
          const slicedStr = str.slice(i, i + 2);
-         const onlyAlphabet = /^[a-zA-Z]+$/;
+         const onlyAlphabet = /[a-zA-Z]{2}/;
          if (onlyAlphabet.test(slicedStr)) array.push(slicedStr.toLowerCase());
     }
     return array;
