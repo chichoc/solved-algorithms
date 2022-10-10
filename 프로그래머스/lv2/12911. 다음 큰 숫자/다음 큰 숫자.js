c23b630eb5c16c761fcf13541bf56e2979355132
chichoc <61962901+chichoc@users.noreply.github.com>
2022-10-10 22:44:08 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,10 +1,9 @@
 function solution(n) {
     const countNum = count(n);
-    while(true) {
-        n++;
+    while(n++) {
         if (countNum === count(n)) return n;
     }
 }
 function count(num) {
-    return [...num.toString(2)].filter(b => b === '1').length;
+    return num.toString(2).match(/1/g).length;
 }
