e7e82bb2bb1c04575e6b57d70dd87e20e77cb1a2
chichoc <61962901+chichoc@users.noreply.github.com>
2022-10-14 00:03:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,6 +1,12 @@
-function solution(n, a = n + 1) {
-    return count(n) === count(a) ? a : solution(n, a + 1);
-}
-function count(num) {
-    return num.toString(2).match(/1/g).length;
+function solution(n) {
+    let i, j;
+    for (i = 0; !(n & 1); i++) n = n >> 1; 
+    // 1을 찾을때까지 우로 쉬프트, 쉬프트 횟수 = i
+    for (j = 0; n & 1; i++, j++) n = n >> 1;
+    // 0을 찾을때까지 우로 쉬프트, 1의 갯수 = j
+    for (j--, n++; i !== j; i--) n = n << 1;
+    // 0자리에 1대입, 1의 갯수 -1, i === j 가 될때까지 죄로 쉬프트하면서 쉬프트 횟수 -1
+    for (; i; i--, n++) n = n << 1;
+    // i === 0 될때까지 좌로 쉬프트 하면서 쉬프트 횟수 -1, 0자리에 1대입
+    return n;
 }
