1bb2f6ce4314f619869bfca58a9edd1614379581
chichoc <61962901+chichoc@users.noreply.github.com>
2022-10-18 23:49:29 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,8 +1,6 @@
 function solution(citations) {
     citations.sort((a, b) => b - a);
-    for (let citation = citations.length; citation > 0; citation--) {
-        const h = citations.filter(c => c >= citation).length;
-        if (h >= citation) return citation;
-    }
-    return 0;
+    let i = 0;
+    while(i + 1 <= citations[i]) i++;
+    return i;
 }
