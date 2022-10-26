d37efb2591ce9c93cbd6754893f1b1e1ffcc0dd1
chichoc <61962901+chichoc@users.noreply.github.com>
2022-10-26 21:44:33 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,16 +1,21 @@
 function solution(n, computers) {
     let answer = 0;
     const isVisited = new Array(n).fill(false);
-    function dfs(currCom) {
-        isVisited[currCom] = true;
-        for (const [i, v] of computers[currCom].entries()) {
-            if (i === currCom || isVisited[i] || v === 0) continue;
-            dfs(i);
+    function bfs(com) {
+        isVisited[com] = true;
+        const queue = [com];
+        while (queue.length > 0) {
+            currCom = queue.pop();
+            isVisited[currCom] = true;
+            for (let i = 0; i < n; i++) {
+                if (isVisited[i] || computers[currCom][i] === 0) continue;
+                queue.push(i);
+            }
         }
     }
     for (let i = 0; i < n; i++){
         if (isVisited[i]) continue;
-        dfs(i);
+        bfs(i);
         answer++;
     }
     return answer;
