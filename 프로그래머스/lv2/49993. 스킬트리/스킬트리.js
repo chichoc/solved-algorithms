d2dddd135b4ca8da4caa3cab2307f89e7e44da02
chichoc <61962901+chichoc@users.noreply.github.com>
2022-10-27 14:22:57 +0900

    [level 2] Title: 스킬트리, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,9 +1,10 @@
 function solution(skill, skill_trees) {
     function isCorrect(tree) {
-        let test = skill.split('');
-        for (var i = 0; i < tree.length; i++) {
+        let skillArr = skill.split('');
+        for (const t of tree){
             // skill에 해당하지 않거나 skill 순서에 맞게 해당된 경우
-            if (!skill.includes(tree[i]) || tree[i] === test.shift()) continue;
+            if (!skill.includes(t) || t === skillArr.shift()) continue;
+            if (skillArr.length < 0) break;
             return false;
         }
         return true;
