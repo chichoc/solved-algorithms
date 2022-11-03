47dc3d0a3bf49215a502b1f476ae82faf1a1150c
chichoc <61962901+chichoc@users.noreply.github.com>
2022-11-03 23:42:22 +0900

    [level 1] Title: 폰켓몬, Time: 0.32 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
     const maxCount = nums.length / 2;
-    const numsMapLength = [...new Set(nums)].length;
-    return maxCount / numsMapLength >= 1 ? numsMapLength : maxCount % numsMapLength;
+    const setOfNumsLen = new Set(nums).size;
+    return maxCount < setOfNumsLen ? maxCount: setOfNumsLen;
 }
