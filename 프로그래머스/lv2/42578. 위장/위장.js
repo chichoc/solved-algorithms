7bb6a4d1eb0d2e98ca07b3fd55f6a4b7554dadd5
chichoc <61962901+chichoc@users.noreply.github.com>
2022-11-06 23:52:29 +0900

    [level 2] Title: 위장, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,6 +1,6 @@
 function solution(clothes) {
-    const clothesLen = {};
-    clothes.forEach(c => clothesLen[c[1]] ? clothesLen[c[1]]++ : clothesLen[c[1]] = 1);
-    return Object.values(clothesLen).reduce((sum, curr) => sum * (curr + 1), 1) - 1;
+    const LengthOfClothes= {};
+    clothes.forEach(([clothing, type]) => LengthOfClothes[type] = (LengthOfClothes[type] || 0) + 1);
+    return Object.values(LengthOfClothes).reduce((sum, curr) => sum * (curr + 1), 1) - 1;
     // (계산된 경우의 수) * (해당 종류의 개수 + 1(착용하지 않는 경우))  - 1(모두 착용하지 않는 경우)
 }
