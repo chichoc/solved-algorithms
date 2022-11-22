a4a569ab1953a0cca12553dc80829efd69fbfe20
chichoc <61962901+chichoc@users.noreply.github.com>
2022-11-22 23:21:28 +0900

    [level 2] Title: 괄호 변환, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -1,33 +1,15 @@
-function solution(p) {
-    let answer = '';
-    let count = {};
-    
-    function makeCorrect(str) {
-        // 1단계
-        if (str === '') return '';
-        // 2단계
-        count = {'(': 0, ')': 0};
-        const indexToSplit = [...str].findIndex((char, index) => {
-            count[char]++;
-            if (count['(']=== count[')']) return true;
-        });
-        const [u, v] = [str.substring(0, indexToSplit + 1), str.substring(indexToSplit + 1)];
-        // 3단계
-        count = {'(': 0, ')': 0};
-        if ([...u].every(c => {
-            count[c]++;
-            return (count['('] < count[')']) ? false :  true;
-        })) {
-            answer += u;
-            makeCorrect(v);
-        } else {
-        // 4단계
-            answer += '(';
-            makeCorrect(v);
-            answer += ')'+ u.slice(1, u.length - 1).split('').map(c => c === '(' ? ')' : '(').join('');
-        }
-    }
-    makeCorrect(p);
-    return answer;
+function reverse(str) {
+    return str.slice(1, str.length - 1).split('').map(c => c === '(' ? ')' : '(').join('')
 }
-
+function solution(p) {
+    // 1단계
+    if (p === '') return '';
+    // 2단계
+    let balance = 0;
+    let pivot = 0;
+    do { balance += p[pivot++] === '(' ? 1 : -1 } while (balance !== 0);
+    const [u, v] = [p.slice(0, pivot), solution(p.slice(pivot))];
+    // 3-4단계 (균형잡힌 괄호 문자열은 (로 시작하면 무조건 올바른 괄호 문자열)
+    if (u[0] ==='(') return u + v;
+    else return '(' + v + ')'+ reverse(u);
+}
