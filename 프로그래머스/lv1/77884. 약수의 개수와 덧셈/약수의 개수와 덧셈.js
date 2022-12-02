3d3200fc6efcb0e380688d8e64e02fa01766c838
chichoc <61962901+chichoc@users.noreply.github.com>
2022-12-03 00:18:54 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,8 +1,7 @@
 function solution(left, right) {
-    let answer = 0;
-    for (let i = left; i <= right; i++) {
-        if (Math.sqrt(i) % 1 === 0) answer -= i;
-        else answer += i
-    }
-    return answer;
+    // 모두 더하고 제곱수를 두 번 빼기 (제곱수가 처음에 더해졌으므로)
+    let sum = (left + right) / 2 * (right - left + 1);
+    let i = Math.ceil(Math.sqrt(left));
+    while (i ** 2 <= right) sum -= (i++ ** 2) * 2
+    return sum;
 }
