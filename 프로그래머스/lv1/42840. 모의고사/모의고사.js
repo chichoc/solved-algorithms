21f1ef80feb07fd1cf34ea38688ae08206945b4b
chichoc <61962901+chichoc@users.noreply.github.com>
2022-12-13 23:52:12 +0900

    [level 1] Title: 모의고사, Time: 4.66 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -4,16 +4,12 @@ function solution(answers) {
     const person1 = [1, 2, 3, 4, 5];
     const person2 = [2, 1, 2, 3, 2, 4, 2, 5];
     const person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    const index = [0, 0, 0];
-    for (const answer of answers) {
-        [person1, person2, person3].forEach((p, i) => (p[index[i]] === answer) && count[i]++);
-        index.forEach((i, idx) => {
-            if (idx === 0 && i === 4) index[0] = 0;
-            else if (idx === 1 && i === 7) index[1] = 0;
-            else if (idx === 2 && i === 9) index[2] = 0;
-            else index[idx]++;
-        });
+    
+    for (let i = 0; i < answers.length; i++) {
+        [person1, person2, person3].forEach((p, pi) => (p[i % p.length] === answers[i]) && count[pi]++);
     }
-    count.forEach((c, i) => c === Math.max(...count) && answer.push(i + 1));
-    return answer.sort((a, b) => a - b);
+    
+    const max = Math.max(...count);
+    count.forEach((c, i) => c === max && answer.push(i + 1));
+    return answer;
 }
