80523ca062363acf9770a4568d4ca9c2ba6f4c6e
chichoc <61962901+chichoc@users.noreply.github.com>
2022-12-23 00:12:40 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
@@ -2,6 +2,7 @@ function solution(w, h) {
     let c = 0;
     for (let i = 0; i < w; i++) {
         c += Math.ceil(h * (i + 1) / w) - Math.floor(h * i / w);
+        // 자바스크립트는 64bit형식의 IEEE 754 기반이라 소수점 정밀도 부분에서 오류 발생 (테스트 6 실패)
     }
     return w * h - c;
 }
