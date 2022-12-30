54a8fc9c68503ac2a3475d104046457131f28845
chichoc <61962901+chichoc@users.noreply.github.com>
2022-12-30 23:53:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,16 +1,7 @@
 function solution(arr) {
-    let answer = arr[0];
-    for (let i = 1; i < arr.length ; i++) {
-        answer = arr[i] * answer / GCD(arr[i], answer);
-    }
-    return answer;
+    return arr.reduce((answer, num) => num * answer / GCD(num, answer));
 }
 
 function GCD(num1, num2) {
-    while (num2 > 0) {
-        const r = num1 % num2;
-        num1 = num2;
-        num2 = r;
-    }
-    return num1;
+    return num1 % num2 ? GCD(num2, num1 % num2) : num2;
 }
