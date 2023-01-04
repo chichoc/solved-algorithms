9a098e5ac8dd4f6b3386307f496660b3e03829d8
chichoc <61962901+chichoc@users.noreply.github.com>
2023-01-05 02:28:41 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.29 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,16 +1,3 @@
 function solution(arr1, arr2) {
-    const answer = [];
-    
-    for (let i = 0; i < arr1.length; i++) {
-        const result = [];
-        for (let j = 0; j < arr2[0].length; j++) {
-            let sum = 0;
-            for (let k = 0; k < arr2.length; k++) {
-                sum += arr1[i][k] * arr2[k][j];
-            }
-            result.push(sum)
-        }
-        answer.push(result);
-    }
-    return answer;
+    return arr1.map(row => arr2[0].map((_, j) => row.reduce((sum, elem, k) => sum + elem * arr2[k][j], 0)));
 }
