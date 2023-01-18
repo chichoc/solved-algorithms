9225d6635ac86aadc28b8a8e59cedb4fec59150d
chichoc <61962901+chichoc@users.noreply.github.com>
2023-01-18 23:46:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,3 +1,8 @@
 function solution(n){
-    return Array.from(n.toString(2), num => +num).reduce((a, b) => a + b);
+    let ans = 0; 
+    while (n > 0) {
+        n &= (n - 1); 
+        ans++; 
+    } 
+    return ans;
 }
