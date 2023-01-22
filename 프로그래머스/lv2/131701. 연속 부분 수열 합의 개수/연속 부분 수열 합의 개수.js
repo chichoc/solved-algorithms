0c841fa43d9e7fb0377c580aa6fa1e0786a8b434
chichoc <61962901+chichoc@users.noreply.github.com>
2023-01-23 00:23:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 759.79 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,11 +1,9 @@
 function solution(elements) {
     const sumSet = new Set(elements);
     for (let i = 2; i <= elements.length; i++) {
-        let end = 0;
+        const extendedElements = elements.concat(elements.slice(0, i));
         for (let start = 0; start < elements.length; start++) {
-            end = start + i;
-            const arrToAdd = (end > elements.length) ? elements.slice(start).concat(elements.slice(0, end - elements.length)) : elements.slice(start, end);
-            sumSet.add(arrToAdd.reduce((sum, curr) => sum + curr, 0));
+            sumSet.add(extendedElements.slice(start, start + i).reduce((sum, curr) => sum + curr, 0));
         }
     }
     return sumSet.size;
