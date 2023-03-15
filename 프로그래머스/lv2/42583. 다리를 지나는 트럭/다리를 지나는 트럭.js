1599ff21531055d941ae6505d32398d254575a02
chichoc <61962901+chichoc@users.noreply.github.com>
2023-03-16 01:48:52 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,18 +1,18 @@
 function solution(bridge_length, weight, truck_weights) {
+    const passing = [];
     let sec = 0;
-    const currentWeight = [];
-    let sumOfCurrentWeight = 0;
+    let sumOfPassing = 0;
     
-    while (truck_weights.length || currentWeight.length) {
-        if (weight >= sumOfCurrentWeight + truck_weights[0] && currentWeight.length <= bridge_length) {
-            const truck = truck_weights.shift();
-            currentWeight.push([truck, sec + bridge_length]);
-            sumOfCurrentWeight += truck;
+    while (truck_weights.length || passing.length) {
+        if (weight >= sumOfPassing + truck_weights[0]) {
+            sumOfPassing += truck_weights[0];
+            passing.push([truck_weights.shift(), sec + bridge_length]);
             sec++;
         } else {
-            const [shiftedWeight, shiftedSec] = currentWeight.shift();
-            sumOfCurrentWeight -= shiftedWeight;
+            const [shiftedWeight, shiftedSec] = passing.shift();
+            sumOfPassing -= shiftedWeight;
             if (shiftedSec > sec) sec = shiftedSec;
+            // 앞에 트럭이 나갈 때 까지 못 들어오므로 나가는 시간으로 점프
         }
     }
     return sec + 1;
