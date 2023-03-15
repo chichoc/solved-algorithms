174028140f541a448d01e9edad910199da409b3d
chichoc <61962901+chichoc@users.noreply.github.com>
2023-03-15 17:17:13 +0900

    [level 2] Title: 모음 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,7 @@
 function solution(word) {
-    let answer = 0;
     const order = { A: 0, E: 1, I: 2, O: 3, U: 4};
-    const wordLen = word.length;
-    
-    for (let i = 0; i < wordLen; i++) {
-        answer += 1 + order[word[i]] * (5 ** (5 - i) - 1) / 4
-    }
-    return answer;
+    return [...word].reduce((sum, char, idx) => sum + 1 + order[char] * (5 ** (5 - idx) - 1) / 4, 0);
 }
 
-// A  AA  AAA  AAAA  AAAAA AAAAE
+// A  AA  AAA  AAAA  AAAAA
 // 1 + 5 + 25 + 125 + 625
