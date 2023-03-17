6f2f8aa8c7c84f1fee7b20720b9394c2c13306c0
chichoc <61962901+chichoc@users.noreply.github.com>
2023-03-17 13:40:48 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 132 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.js
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.js
@@ -1,27 +1,11 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().split(' ');
 
-n = +input[0];
-let countFib = 0;
-let countFibonacci = 0;
+const n = +input[0];
+const nums = [0, 1];
 
-fib = (n) => {
-  if (n == 1 || n == 2) {
-    countFib++;
-    return 1; // 코드1
-  } else return fib(n - 1) + fib(n - 2);
-};
+for (let i = 1; i < n; i++) {
+  nums.push(nums[nums.length - 1] + nums[nums.length - 2]);
+}
 
-fibonacci = (n) => {
-  const f = [0, 1, 1];
-  for (let i = 3; i <= n; i++) {
-    countFibonacci++;
-    f[i] < -f[i - 1] + f[i - 2]; // 코드2
-  }
-  return f[n];
-};
-
-fib(n);
-fibonacci(n);
-
-console.log(countFib, countFibonacci);
+console.log(nums.pop(), n - 2);
