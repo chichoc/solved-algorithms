d23ab936d109321475f3314f472163462330c709
chichoc <61962901+chichoc@users.noreply.github.com>
2023-03-18 22:12:56 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 832 ms, Memory: 17268 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.js
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().split('\n');
 
-const inputs = input.map((string) => string.split(' ').map((num) => +num));
-const cache = Array.from({ length: 21 }, () => Array.from({ length: 21 }, () => []));
+const inputs = input.map((string) => string.split(' ').map(Number));
+const cache = [...Array(21)].map(() => Array.from({ length: 21 }, () => Array(21)));
 
-const w = (a, b, c) => {
-  if (a <= 0 || b <= 0 || c <= 0) return 1;
-  if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
-  if (cache[a][b][c]) return cache[a][b][c];
-  else if (a < b && b < c) {
-    return (cache[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c));
-  } else return (cache[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1));
+for (let a = 0; a < 21; a++) {
+  for (let b = 0; b < 21; b++) {
+    for (let c = 0; c < 21; c++) {
+      if (a <= 0 || b <= 0 || c <= 0) cache[a][b][c] = 1;
+      else if (a < b && b < c) cache[a][b][c] = cache[a][b][c - 1] + cache[a][b - 1][c - 1] - cache[a][b - 1][c];
+      else
+        cache[a][b][c] =
+          cache[a - 1][b][c] + cache[a - 1][b - 1][c] + cache[a - 1][b][c - 1] - cache[a - 1][b - 1][c - 1];
+    }
+  }
+}
+
+w = (a, b, c) => {
+  if (a <= 0 || b <= 0 || c <= 0) return cache[0][0][0];
+  else if (a > 20 || b > 20 || c > 20) return cache[20][20][20];
+  else return cache[a][b][c];
 };
 
 for (const [a, b, c] of inputs) {
