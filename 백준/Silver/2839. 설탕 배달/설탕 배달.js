b8da12eec2ceaa66535d7e5d1254373174393259
chichoc <61962901+chichoc@users.noreply.github.com>
2023-03-21 23:09:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -2,17 +2,13 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().split('\n');
 
 const N = +input[0];
-const cache = Array(N + 1).fill(0);
+const cache = Array(N + 1).fill(-1);
 cache[3] = cache[5] = 1;
 
-dp = (weight) => {
-  if (cache[weight] === 0) {
-    if (weight % 5 === 0) cache[weight] = dp(weight - 5) + 1;
-    else if (weight % 3 === 0) cache[weight] = dp(weight - 3) + 1;
-    else if (dp(weight - 5) > 0 && dp(weight - 3) > 0) cache[weight] = Math.min(dp(weight - 5), dp(weight - 3)) + 1;
-    else cache[weight] = -1;
-  }
-  return cache[weight];
-};
+for (let i = 6; i <= N; i++) {
+  if (i % 5 === 0) cache[i] = cache[i - 5] + 1;
+  else if (i % 3 === 0) cache[i] = cache[i - 3] + 1;
+  else if (cache[i - 5] > 0 && cache[i - 3] > 0) cache[i] = Math.min(cache[i - 5], cache[i - 3]) + 1;
+}
 
-console.log(dp(N));
+console.log(cache[N]);
