1cc75e10e1a6e8406d34e8e016c68e337f2ddb41
chichoc <61962901+chichoc@users.noreply.github.com>
2023-03-22 23:52:33 +0900

    [level 1] Title: 최소직사각형, Time: 2.04 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,10 @@
+function solution(sizes) {
+    const w = [];
+    const h = [];
+    
+    sizes.forEach(size => {
+        w.push(Math.max(...size));
+        h.push(Math.min(...size))
+    })
+    return Math.max(...w) * Math.max(...h);
+  }
