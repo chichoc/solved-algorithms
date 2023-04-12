250c6f017438660b1e5f28c447719daab19e4d45
chichoc <61962901+chichoc@users.noreply.github.com>
2023-04-12 13:53:08 +0900

    [Silver IV] Title: 수 찾기, Time: 288 ms, Memory: 38712 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,24 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().split('\n');
 
-const [N, A, M, nums] = input;
-const a = A.split(' ')
-  .sort((a, b) => a - b)
-  .map(Number);
-
-function binarySearch(target, start, end) {
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    if (a[mid] === target) return 1;
-    else if (a[mid] > target) end = mid - 1;
-    else start = mid + 1;
-  }
-  return 0;
-}
-
+const a = new Set(input[1].split(' '));
 console.log(
-  nums
+  input[3]
     .split(' ')
-    .map((num) => binarySearch(+num, 0, N - 1))
+    .map((str) => (a.has(str) ? 1 : 0))
     .join('\n')
 );
