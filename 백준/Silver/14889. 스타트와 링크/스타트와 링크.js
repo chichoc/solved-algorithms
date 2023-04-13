113370a7c29c4b8995dec2ed0f625b9e6a4d8951
chichoc <61962901+chichoc@users.noreply.github.com>
2023-04-14 01:40:35 +0900

    [Silver II] Title: 스타트와 링크, Time: 764 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
@@ -1,45 +1,39 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().split('\n');
-
+const input = require('fs').readFileSync(0).toString().split('\n');
 const N = +input[0];
 const skills = [];
+const allNums = [...Array(N).keys()];
 let minDiff = 100;
 
 for (let i = 1; i < N + 1; i++) {
   skills.push(input[i].split(' ').map(Number));
 }
 
-function getComb(arr, count) {
-  const results = [];
-  if (count === 1) return arr.map((el) => [el]);
+function teamCompare(arr1, arr2) {
+  let score1 = 0;
+  let score2 = 0;
 
-  arr.forEach((fixed, index, origin) => {
-    const rest = origin.slice(index + 1);
-    const combinations = getComb(rest, count - 1);
-    const attached = combinations.map((el) => [fixed, ...el]);
-    results.push(...attached);
-  });
-  return results;
+  for (let i = 0; i < arr1.length - 1; i++) {
+    for (let j = i + 1; j < arr1.length; j++) {
+      score1 += skills[arr1[i]][arr1[j]] + skills[arr1[j]][arr1[i]];
+      score2 += skills[arr2[i]][arr2[j]] + skills[arr2[j]][arr2[i]];
+    }
+  }
+  return Math.abs(score1 - score2);
 }
 
-function sum(arr) {
-  return getComb(arr, 2).reduce((sum, [n1, n2]) => sum + skills[n1][n2] + skills[n2][n1], 0);
-}
-
-const allNums = [...Array(N).keys()];
-function bfs(nums) {
+function dfs(nums) {
   if (nums.length === N / 2) {
     const rest = allNums.filter((key) => !nums.includes(key));
-    const diff = Math.abs(sum(nums) - sum(rest));
+    const diff = teamCompare(nums, rest);
     if (minDiff > diff) minDiff = diff;
   }
   const lastNum = nums[nums.length - 1];
   for (let i = lastNum + 1; i < N; i++) {
     if (nums.includes(i)) continue;
-    bfs([...nums, i]);
+    dfs([...nums, i]);
   }
 }
 
-bfs([0]);
+dfs([0]);
 
 console.log(minDiff);
