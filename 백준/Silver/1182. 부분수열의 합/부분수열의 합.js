8940682f90a950c1dba81fb5386e9a08648dfb17
chichoc <61962901+chichoc@users.noreply.github.com>
2023-04-15 00:43:44 +0900

    [Silver II] Title: 부분수열의 합, Time: 204 ms, Memory: 9860 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,22 +1,15 @@
-const input = require('fs').readFileSync(0).toString().split('\n');
-const [N, S] = input[0].split(' ').map(Number);
-const nums = input[1].split(' ').map(Number);
-
-const selectedNums = [];
+const [N, S, ...nums] = require('fs').readFileSync(0).toString().split(/\s+/).map(Number);
 let count = 0;
 
-dfs(0);
-console.log(S === 0 ? count - 1 : count);
+dfs(0, 0);
+console.log(count);
 
-function dfs(depth) {
-  if (depth === N) {
-    const sum = selectedNums.reduce((sum, num) => sum + num, 0);
-    if (sum === S) count++;
-    return;
-  }
+function dfs(depth, sum) {
+  if (depth === N) return;
 
-  selectedNums.push(nums[depth]);
-  dfs(depth + 1);
-  selectedNums.pop();
-  dfs(depth + 1);
+  sum += nums[depth];
+  if (sum === S) count++;
+
+  dfs(depth + 1, sum);
+  dfs(depth + 1, sum - nums[depth]);
 }
