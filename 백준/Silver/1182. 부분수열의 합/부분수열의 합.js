bfe3eee1c4ef263f14d0119f9d69e406aa875d97
chichoc <61962901+chichoc@users.noreply.github.com>
2023-04-15 08:48:33 +0900

    [Silver II] Title: 부분수열의 합, Time: 188 ms, Memory: 9908 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -4,12 +4,10 @@ let count = 0;
 dfs(0, 0);
 console.log(count);
 
-function dfs(depth, sum) {
-  if (depth === N) return;
+function dfs(sum, start) {
+  if (start > 0 && sum === S) count++;
 
-  sum += nums[depth];
-  if (sum === S) count++;
-
-  dfs(depth + 1, sum);
-  dfs(depth + 1, sum - nums[depth]);
+  for (let i = start; i < N; i++) {
+    dfs(sum + nums[i], ++start);
+  }
 }
