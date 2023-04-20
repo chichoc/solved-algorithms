bbaaccf064b12eb89d64d58cfdbd437705419ea5
chichoc <61962901+chichoc@users.noreply.github.com>
2023-04-20 15:28:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 204 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,40 +1,42 @@
-const [input, ...connects] = require('fs').readFileSync(0).toString().trim().split('\n'); //제출용
-const [N, M, V] = input.split(' ').map(Number);
+const input = require('fs').readFileSync(0).toString().trim().split('\n'); //제출용
+const [N, M, V] = input[0].split(' ').map(Number);
 
 const graph = Array.from({ length: N + 1 }, () => []);
-const visitedDfs = Array(N + 1).fill(0);
-const visitedBfs = Array(N + 1).fill(0);
+const visitedDfs = Array(N + 1).fill(false);
+const visitedBfs = Array(N + 1).fill(false);
 const resultDfs = [];
 const resultBfs = [];
 const queueBfs = [V];
 
-connects.forEach((connect) => {
-  const [num1, num2] = connect.split(' ');
+for (let i = 1; i <= M; i++) {
+  const [num1, num2] = input[i].split(' ').map(Number);
   graph[num1].push(num2);
   graph[num2].push(num1);
-});
+}
+
+graph.forEach((edges) => edges.sort((a, b) => a - b));
 
 function dfs(num) {
   resultDfs.push(num);
-  visitedDfs[num] = 1;
-  graph[num].sort((a, b) => a - b);
+  visitedDfs[num] = true;
+
   for (const numToMove of graph[num]) {
-    if (visitedDfs[numToMove] === 1) continue;
+    if (visitedDfs[numToMove]) continue;
     dfs(numToMove);
   }
 }
 dfs(V);
 
 // bfs
-while (queueBfs.length > 0) {
+visitedBfs[V] = true;
+while (queueBfs.length) {
   const num = queueBfs.shift();
-  if (visitedBfs[num] === 0) {
-    resultBfs.push(num);
-    visitedBfs[num] = 1;
-  }
+  resultBfs.push(num);
+
   for (const numToMove of graph[num]) {
-    if (visitedBfs[numToMove] === 1) continue;
+    if (visitedBfs[numToMove]) continue;
     queueBfs.push(numToMove);
+    visitedBfs[numToMove] = true;
   }
 }
 
