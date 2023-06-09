0c1b2d23cccb6d77a70261b400a401b7285655fc
chichoc <61962901+chichoc@users.noreply.github.com>
2023-06-09 11:13:45 +0900

    [Gold IV] Title: 우체국, Time: 432 ms, Memory: 46268 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.js
+++ 백준/Gold/2141. 우체국/우체국.js
@@ -1,9 +1,8 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 
 const N = +inputs[0];
-const answer = [];
 const XandA = inputs.slice(1).map((input) => input.split(' ').map(Number));
-const totalA = XandA.reduce((sum, curr) => sum + curr[1], 0);
+const halfOfTotalA = XandA.reduce((sum, curr) => sum + curr[1], 0) / 2;
 let sum = 0;
 
 // 마을 번호 기준 오름차순 정렬
@@ -11,6 +10,6 @@ XandA.sort((a, b) => a[0] - b[0]);
 
 for (let i = 0; i < N; i++) {
   sum += XandA[i][1];
-  if (totalA / 2 <= sum) return console.log(XandA[i][0]);
+  if (halfOfTotalA <= sum) return console.log(XandA[i][0]);
 }
 console.log(XandA[N - 1][0]);
