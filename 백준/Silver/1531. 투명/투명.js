5538ea4d73c99550812a049332e4693446a34e88
chichoc <61962901+chichoc@users.noreply.github.com>
2023-07-14 21:17:09 +0900

    [Silver V] Title: 투명, Time: 180 ms, Memory: 10128 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.js
@@ -0,0 +1,27 @@
+const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
+const M = +inputs[0].split(' ')[1];
+const papers = inputs.slice(1).map((paper) => paper.split(' ').map(Number));
+const pictures = Array.from({ length: 101 }, () => Array(101).fill(0));
+let answer = 0;
+
+// 주어진 종이들로 가리기
+for (const paper of papers) {
+  cover(...paper);
+}
+
+// M+1개 가려져 보이지 않는 그림의 개수 세기
+for (let r = 0; r <= 100; r++) {
+  for (let c = 0; c <= 100; c++) {
+    if (pictures[r][c] > M) answer++;
+  }
+}
+
+function cover(r1, c1, r2, c2) {
+  for (let r = r1; r <= r2; r++) {
+    for (let c = c1; c <= c2; c++) {
+      pictures[r][c]++;
+    }
+  }
+}
+
+console.log(answer);
