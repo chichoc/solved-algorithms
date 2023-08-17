d46a73e6bd0f70519315f8c358b346cbe2510bfc
chichoc <61962901+chichoc@users.noreply.github.com>
2023-08-17 16:59:33 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,14 +1,12 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const N = +inputs[0];
 const times = inputs[1].split(' ').map(Number);
-let sum = 0;
 let answer = 0;
 
 times.sort((a, b) => a - b);
 
 for (let i = 0; i < N; i++) {
-  answer += sum + times[i];
-  sum += times[i];
+  answer += times[i] * (N - i);
 }
 
 console.log(answer);
