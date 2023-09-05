e829b99395446b38dd8d0f2b3ad9b86765aae422
chichoc <61962901+chichoc@users.noreply.github.com>
2023-09-05 19:48:19 +0900

    [level 3] Title: 여행경로, Time: 3.44 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -0,0 +1,47 @@
+function solution(tickets) {
+    const answers = [];
+    const arrival = {};
+    const lenOfAnswer = tickets.length + 1;
+    
+    // 객체로 저장
+    for (const ticket of tickets) {
+        const [start, end] = ticket;
+        
+        if (arrival[start]) arrival[start].push(end);
+        else arrival[start] = [end];
+        
+        if (!arrival[end]) arrival[end] = [];
+    }
+    
+    
+    // 모두 방문하는 공항경로 저장
+    dfs('ICN', 1, ['ICN'], {...arrival});
+    
+    // 알파벳 순서 비교
+    
+    console.log(answers)
+    return getFirstAlphabetically();
+    
+    
+    function dfs(curr, count, course, arrivalToVisit) {
+        if (count === lenOfAnswer) {
+            answers.push(course);
+            return;
+        };
+        for (const [idx, airport] of Object.entries(arrivalToVisit[curr])) {
+            const newArrivalToVisit = JSON.parse(JSON.stringify(arrivalToVisit));
+            newArrivalToVisit[curr].splice(idx, 1);
+            dfs(airport, count + 1, [...course, airport], newArrivalToVisit);
+        }
+    }
+    
+    function getFirstAlphabetically() {
+        let firstAlphabetically = answers[0];
+        for (let i = 1; i < answers.length; i++) {
+            const currentArray = answers[i];
+            if (JSON.stringify(currentArray) < JSON.stringify(firstAlphabetically)) firstAlphabetically = currentArray;
+        }
+        return firstAlphabetically;
+    }
+    
+}
