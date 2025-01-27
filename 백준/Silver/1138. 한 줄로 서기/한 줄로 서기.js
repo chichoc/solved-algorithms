b9115d492c99d9ff83f01d764419406b82a20c62
chichoc <chichoc.dev@gmail.com>
2025-01-27 17:46:51 +0900

    [Silver II] Title: 한 줄로 서기 - 이유 및 시도코드 추가

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
@@ -15,6 +15,12 @@ for (let i = 0; i < N; i++) {
     }
     idx++;
   }
+  // const otherPos = pos + answer.slice(0, pos).filter((num) => num > 0).length;
+  // const move = answer.slice(otherPos).indexOf(0) + otherPos;
+
+  // let otherPos = answer.slice(pos).indexOf(0) + pos;
+  // const move = pos - answer.slice(0, otherPos).filter((num) => !num).length;
+  // if (move > 0) otherPos += move;
   answer[idx] = i + 1;
 }
 
@@ -23,3 +29,4 @@ console.log(answer.join(' '));
 // 반례: 10 \n 5 3 7 1 4 2 1 0 0 0
 // 정답: 8 4 7 2 6 1 9 5 10 3
 // 출력: 8 4 6 2 5 1 7 3 9 10
+// 헤맨 점: 너무 복잡하게 구현함 (while 쓰는게 훨 가독성 좋음)
