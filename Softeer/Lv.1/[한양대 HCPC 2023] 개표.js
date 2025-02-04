ef19aa5e19152866090091591ac5726cee80f219
chichoc <chichoc.dev@gmail.com>
2025-02-04 17:32:30 +0900

    [Softeer_Lv.1] Title: [한양대 HCPC 2023] 개표, Time: 45 ms, Memory: 6.98 MB - 식별자명 수정 & 함수 로직 추가

--- Softeer/Lv.1/[한양대 HCPC 2023] 개표.js
+++ Softeer/Lv.1/[한양대 HCPC 2023] 개표.js
@@ -1,16 +1,18 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const T = +inputs[0];
-const five = '++++ ';
-const one = '|';
+const fiveUnit = '++++ ';
+const oneUnit = '|';
 let answer = '';
 
-function divideFive(num) {
-  return [Math.floor(num / 5), num % 5];
+function convertScoreToSymbols(score) {
+  const fiveCount = Math.floor(score / 5);
+  const oneCount = score % 5;
+  return fiveUnit.repeat(fiveCount) + oneUnit.repeat(oneCount);
 }
 
 for (let i = 1; i <= T; i++) {
-  const [m, n] = divideFive(+inputs[i]);
-  answer += five.repeat(m) + one.repeat(n) + '\n';
+  const [m, n] = convertScoreToSymbols(+inputs[i]);
+  answer += convertScoreToSymbols(+inputs[i]) + '\n';
 }
 
 console.log(answer);
