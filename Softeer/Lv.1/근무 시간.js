5abad7d810a0801af6ea6339fbd7e71db6617030
chichoc <chichoc.dev@gmail.com>
2025-02-04 15:57:05 +0900

    [Softeer_Lv.1] Title: 근무시간, Time: 48 ms, Memory: 7.20 MB - 반복되는 H:M 분리 과정 함수에 추가

--- Softeer/Lv.1/근무 시간.js
+++ Softeer/Lv.1/근무 시간.js
@@ -1,15 +1,14 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 let answer = 0;
 
-function countMinute(h, m) {
+function countMinute(string) {
+  const [h, m] = string.split(':').map(Number);
   return h * 60 + m;
 }
 
 for (const input of inputs) {
   const [startTime, endTime] = input.split(' ');
-  const [startHour, startMinute] = startTime.split(':').map(Number);
-  const [endHour, endMinute] = endTime.split(':').map(Number);
-  answer += countMinute(endHour, endMinute) - countMinute(startHour, startMinute);
+  answer += countMinute(endTime) - countMinute(startTime);
 }
 
 console.log(answer);
