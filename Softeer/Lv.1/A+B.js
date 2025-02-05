22a9254a197d8bda1eda6dd1756e8cb4f94405bc
chichoc <chichoc.dev@gmail.com>
2025-02-05 17:53:37 +0900

    [Softeer_Lv.1] Title: A+B, Time: 45 ms, Memory: 7.00 MB - 불필요한 함수 분리 수정 및 문자열 결합 -> 배열

--- Softeer/Lv.1/A+B.js
+++ Softeer/Lv.1/A+B.js
@@ -1,16 +1,15 @@
 // https://softeer.ai/practice/6295
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const T = +inputs[0];
-const cases = inputs.slice(1).map((input) => input.split(' ').map(Number));
-let answer = '';
+const answer = [];
 
 function add(a, b) {
   return a + b;
 }
 
-for (let i = 0; i < T; i++) {
-  const [a, b] = cases[i];
-  answer += `Case #${i + 1}: ${add(a, b)}\n`;
+for (let i = 1; i <= T; i++) {
+  const [a, b] = inputs[i].split(' ').map(Number);
+  answer.push(`Case #${i}: ${add(a, b)}`);
 }
 
-console.log(answer);
+console.log(answer.join('\n'));
