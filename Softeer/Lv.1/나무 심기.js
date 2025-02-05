1005025b918070eee7be3801ba1343438434f011
chichoc <chichoc.dev@gmail.com>
2025-02-05 13:55:24 +0900

    [Softeer_Lv.1] Title: 나무 심기, Time: 44 ms, Memory: 7.05 MB - 불필요한 함수화 제거 및 초기값 하드코딩 수정

--- Softeer/Lv.1/나무 심기.js
+++ Softeer/Lv.1/나무 심기.js
@@ -2,23 +2,12 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const n = +inputs[0];
 const fInfos = inputs[1].split(' ').map(Number);
-let answer = -10000;
+let answer = -Infinity;
 
-function combination(num) {
-  const result = [];
-
-  for (let i = 0; i < num; i++) {
-    for (let j = i + 1; j < num; j++) {
-      result.push([i, j]);
-    }
+for (let i = 0; i < n; i++) {
+  for (let j = i + 1; j < n; j++) {
+    answer = Math.max(answer, fInfos[i] * fInfos[j]);
   }
-  return result;
-}
-
-const combis = combination(n);
-
-for (const [idx1, idx2] of combis) {
-  answer = Math.max(answer, fInfos[idx1] * fInfos[idx2]);
 }
 
 console.log(answer);
