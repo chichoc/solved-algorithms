4a728b6cba3ed7c85aedf7ac03843e83a8771ec7
chichoc <chichoc.dev@gmail.com>
2025-02-05 18:14:13 +0900

    [Softeer_Lv.1] Title: 연탄 배달의 시작, Time: 48 ms, Memory: 7.05 MB

---
+++ Softeer/Lv.1/연탄 배달의 시작.js
@@ -0,0 +1,26 @@
+// https://softeer.ai/practice/7626
+const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
+const n = +inputs[0];
+const positions = inputs[1].split(' ').map(Number);
+let answer = 0;
+let minDiff = Infinity;
+
+for (let i = 0; i < n - 1; i++) {
+  for (let j = i + 1; j < i + 2; j++) {
+    const diff = positions[j] - positions[i];
+    if (diff < minDiff) {
+      answer = 1;
+      minDiff = diff;
+    } else if (diff > minDiff) {
+      continue;
+    } else {
+      answer += 1;
+    }
+  }
+}
+
+console.log(answer);
+/** 회고
+ * IDE 환경이 아니다보니 변수 오타로 헤맴
+ * 입력값이 오름차순이라 모두 순회할 필요 X
+ */
