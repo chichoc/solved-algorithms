3ecf11d4a12b32fe340b1ff4425c60bb14b1f009
chichoc <chichoc.dev@gmail.com>
2025-02-05 17:28:28 +0900

    [Softeer_Lv.1] Title: 위험한 효도, Time: 51 ms, Memory: 6.93 MB - 다른 풀이 참고 후 개선점 정리2

--- Softeer/Lv.1/위험한 효도.js
+++ Softeer/Lv.1/위험한 효도.js
@@ -2,33 +2,24 @@
 const [a, b, d] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
 let answer = 0;
 
-function play(timeFront, timeBack, restD) {
-  let result = 0;
-  let front = false;
-
-  while (restD > 0) {
-    if (front) {
-      result += timeFront;
-      front = false;
-    } else {
-      for (let i = 0; i < timeBack; i++) {
-        restD -= 1;
-        result += 1;
-        if (restD === 0) return result;
-      }
-      front = true;
-    }
-  }
-  return result;
+function play(timeFront, timeBack, distance) {
+  return (timeFront + timeBack) * Math.floor(distance / timeFront) + (distance % timeFront);
 }
 
-answer += play(b, a, d);
-answer += play(a, b, d);
+const beforeTouch = play(b, a, d);
+const afterTouch = play(a, b, d);
+answer = beforeTouch + afterTouch;
 
-console.log(answer);
+console.log(a === d ? answer - b : answer);
 
 /** 다른 풀이1: https://velog.io/@lookin_min/Softeer-%EC%9C%84%ED%97%98%ED%95%9C-%ED%9A%A8%EB%8F%84Python
  내 풀이와 비교
  * 문제를 복잡하게 풂 -> 문제의 핵심 파악하기 & 필요한 상태만 관리
  * 핵심: 앞, 뒤 시간만 다르고 동일한 로직이므로 함수 분리 가능
+ **/
+
+/** 다른 풀이2: https://yunicornlab.tistory.com/22
+ 내 풀이와 비교
+ * 수학적 접근
+ * 핵심: a === d인 경우 술래 터치 후 뒤를 보는 시간이 필요없으므로 -b를 해줌
  **/
