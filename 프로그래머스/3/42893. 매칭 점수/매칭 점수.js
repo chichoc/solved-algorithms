2f9e683fb8cb8ef23a68cb300c55b5e6d2a7fcd8
chichoc <chichoc.dev@gmail.com>
2025-02-28 11:47:09 +0900

    [level 3] Title: 매칭 점수, Time: 2.14 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.js
@@ -0,0 +1,66 @@
+function solution(word, pages) {
+    let answer = 0;
+    const pageLen = pages.length;
+    const pageNames = Array(pageLen).fill('');
+    // const pageInfos = Array.from({length: pageLen}, () => {})
+    const pageInfos = Array.from({length: pageLen}, () => ({inners:[]}))
+    const matchingScore = [];
+    
+    function findAttr(meta, attribute) {
+        const regex = new RegExp(`${attribute}="([^"]+)"`)
+        const match = meta.match(regex);
+        // console.log(meta, match)
+        return match ? match[1].replace('https://', '') : null;
+    }
+    
+    function checkDefaultScore(body) {
+        const regex = new RegExp(`(?:^|[^a-zA-Z])${word}(?=[^a-zA-Z]|$)`, "gi");
+        const result = body.match(regex);
+        return result ? result.length : 0;
+    }
+    // console.log(checkDefaultScore('0muzi0muzi0'))
+    
+    function findLinks(body) {
+        const regexA = /<a[^>]+>/g;
+        const aTags = body.match(regexA);
+        return aTags?.map(tag => findAttr(tag, 'href')) || [];
+    }
+    
+    function calculateLinkScore(i) {
+        return pageInfos[i].default / pageInfos[i].link
+    }
+    
+    for (let i = 0; i < pageLen; i++) {
+        // 웹페이지 url 찾기 (meta 태그의 content 속성)
+        const regexMeta = /<meta[^>]+>/g;
+        const metaTags = pages[i].match(regexMeta);
+        metaTags.forEach(tag => {
+            if (!tag.includes('content')) return true;
+            pageNames[i] = findAttr(tag, 'content');
+        })
+        
+        // 기본 점수 계산
+        pageInfos[i].default = checkDefaultScore(pages[i]);
+    }
+    
+    // 외부 링크 찾기
+    for (let i = 0; i < pageLen; i++) {
+        const links = findLinks(pages[i]);
+        pageInfos[i].link = links.length;
+        links.forEach(link => {
+            const idx = pageNames.indexOf(link);
+            if (idx < 0) return;
+            else pageInfos[pageNames.indexOf(link)].inners.push(i)
+        })
+    }
+    
+    // 링크 & 매칭 점수 계산
+    for (let i = 0; i < pageLen; i++) {
+        let score = pageInfos[i].default;
+        pageInfos[i].inners.forEach(inner => score += calculateLinkScore(inner))
+        matchingScore.push(score);
+    }
+    
+    // console.log(pageNames, pageInfos, matchingScore)
+    return matchingScore.indexOf(Math.max(...matchingScore))
+}
