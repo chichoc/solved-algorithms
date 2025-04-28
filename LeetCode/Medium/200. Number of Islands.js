6ea138591b268df32045842a3cf78a8cdc22e127
chichoc <chichoc.dev@gmail.com>
2025-04-28 17:30:58 +0900

    [medium] Title: 200. Number of Islands, Time: 64ms (Beats 53.54%), Memory: 59.79MB (Beats 53.32%) - 리팩토링 및 회고 추가

--- LeetCode/Medium/200. Number of Islands.js
+++ LeetCode/Medium/200. Number of Islands.js
@@ -5,11 +5,12 @@
  * @return {number}
  */
 
-var numIslands = function (grid) {
-  const rowLen = grid.length;
-  const colLen = grid[0].length;
-  const dr = [-1, 1, 0, 0];
-  const dc = [0, 0, -1, 1];
+const numIslands = function (grid) {
+  const rowLen = grid.length,
+    colLen = grid[0].length;
+  const dr = [-1, 1, 0, 0],
+    dc = [0, 0, -1, 1];
+  const visited = Array.from({ length: rowLen }, () => Array(colLen).fill(false));
   let answer = 0;
 
   const checkGridOver = (row, col) => {
@@ -18,25 +19,27 @@ var numIslands = function (grid) {
     } else return false;
   };
 
-  const findIsland = (row, col, visited) => {
+  const findIsland = (row, col) => {
     visited[row][col] = true;
     for (let i = 0; i < 4; i++) {
       const [nextRow, nextCol] = [row + dr[i], col + dc[i]];
       if (checkGridOver(nextRow, nextCol)) continue;
-      if (visited[nextRow][nextCol]) continue;
-      if (grid[nextRow][nextCol] === '0') continue;
-      findIsland(nextRow, nextCol, visited);
+      if (visited[nextRow][nextCol] || grid[nextRow][nextCol] === '0') continue;
+      findIsland(nextRow, nextCol);
     }
   };
 
-  const visited = Array.from({ length: rowLen }, () => Array(colLen).fill(false));
   for (let r = 0; r < rowLen; r++) {
     for (let c = 0; c < colLen; c++) {
-      if (visited[r][c]) continue;
-      if (grid[r][c] === '0') continue;
-      findIsland(r, c, visited);
+      if (visited[r][c] || grid[r][c] === '0') continue;
       answer++;
+      findIsland(r, c);
     }
   }
   return answer;
 };
+
+/** 회고
+ - 기본적인 dfs 문제
+ - visited[r][c] || grid[r][c] === '0' 반복됨
+ */
