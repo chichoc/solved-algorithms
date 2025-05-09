428b42de95756b317e4bc6c165ef6bf33787db0c
chichoc <chichoc.dev@gmail.com>
2025-05-09 17:42:36 +0900

    [Silver I] Title: 귀여운 라이언, Time: 220 ms, Memory: 36924 KB - 리팩토링 및 회고 추가

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.js
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.js
@@ -3,18 +3,30 @@ const [N, K] = inputs[0].split(' ').map(Number);
 const dolls = inputs[1].split(' ').map(Number);
 const lyan = 1;
 let minSize = Infinity;
-let start = 0;
+let start = 0,
+  end = 0;
 let numberOfLyan = 0;
 
-for (let end = 0; end < N; end++) {
+while (end <= N) {
+  if (numberOfLyan < K) {
     if (dolls[end] === lyan) numberOfLyan++;
+    end++;
+  } else {
+    if (dolls[start] === lyan) numberOfLyan--;
+    start++;
+  }
 
-    while (numberOfLyan === K) {
-      if (dolls[start] === lyan) numberOfLyan--;
-      
-      minSize = Math.min(minSize, end - start + 1);
-      start++;
-    }
+  if (numberOfLyan === K) {
+    minSize = Math.min(minSize, end - start);
+  }
 }
 
 console.log(minSize === Infinity ? -1 : minSize);
+
+/**
+ * 회고
+ * - 투포인터 어떻게 푸는지 몰라서 매번 라이언 위치를 찾고 슬라이싱..그래서 시초->메초?공초? 뜸
+ *   정해진 유형이라 빠르게 파악하는게 중요
+ * - K개 이상이지만 가장 작은 집합의 크기를 구하는거라 K개인 경우만 체크하면 됨
+ * - 백준 기준 가장 빠른 풀이는 이중 반복문이지만 O(N)이 더 효율적이라 생각되어 위 코드로 수정
+ */
