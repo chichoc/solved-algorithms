4d4720f03adeffe94e850054ed4f6f5137da44e9
chichoc <chichoc.dev@gmail.com>
2025-05-21 11:47:43 +0900

    [Silver I] Title: 부분수열의 합, Time: 96 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.js
@@ -1,39 +1,14 @@
-const inputs = require('fs').readFileSync(0).toString().trim().split('\n'); // 입력 여러 줄
+const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const N = +inputs[0];
-const S = inputs[1].split(' ').map(Number);
-const sum = new Set([...S]);
+const S = inputs[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+let sum = 1;
 
-const getSum = (arr) => arr.reduce((curr, sum) => curr + sum, 0);
-
-const getCombination = (arr, count) => {
-  const result = [];
-  if (count === 1) return arr.map((value) => [value]);
-
-  arr.forEach((fixed, idx, origin) => {
-    const rest = origin.slice(idx + 1);
-    const combinations = getCombination(rest, count - 1);
-    const attached = combinations.map((combi) => [fixed, ...combi]);
-    result.push(...attached);
-  });
-
-  return result;
-};
-
-for (let i = 2; i <= N; i++) {
-  if (i === N) {
-    const totalSum = getSum(S);
-    sum.add(totalSum);
-  } else {
-    const newSums = getCombination(S, i);
-    newSums.forEach((sums) => sum.add(getSum(sums)));
-  }
+for (let i = 0; i < N; i++) {
+  if (S[i] > sum) break;
+  sum += S[i];
 }
 
-const maxSum = Math.max(...sum);
-
-for (let i = 1; i <= maxSum; i++) {
-  if (sum.has(i)) continue;
-  return console.log(i);
-}
-
-console.log(maxSum + 1);
+console.log(sum);
