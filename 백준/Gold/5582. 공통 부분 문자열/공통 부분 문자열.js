53143e4b6d38487297795f00835c0d4a830a55ec
chichoc <chichoc.dev@gmail.com>
2025-05-26 17:14:05 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 392 ms, Memory: 10652 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.js
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.js
@@ -8,7 +8,7 @@ let answer = 0;
 const dp = Array(len1).fill(0);
 
 for (let i = 0; i < len2; i++) {
-  for (let j = len1; j >= 0; j--) {
+  for (let j = len1 - 1; j >= 0; j--) {
     if (str2[i] !== str1[j]) dp[j] = 0;
     else {
       if (j === 0) dp[j] = 1;
@@ -18,9 +18,3 @@ for (let i = 0; i < len2; i++) {
   }
 }
 console.log(answer);
-
-/**
- * 처음에 N^3이라 시초
- * DP에 이전 값을 저장해서 N^2로 줄어야 함
- * DP는 이전 값을 참고하므로 1차원에 저장해도 되지만, 갱신되므로 하나는 뒤에서부터 확인해야 함 (둘다 앞에서부터 순회하면 틀림,,,완벽히 이해하지 못함ㅜ)
- */
