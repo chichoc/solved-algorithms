54337837ed11bd25a485ff2d7a90448931f952c1
chichoc <chichoc.dev@gmail.com>
2025-05-26 16:46:10 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 528 ms, Memory: 11148 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.js
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.js
@@ -1,17 +1,19 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
-const str1 = inputs[0];
-const str2 = inputs[1];
-const len1 = str1.length;
-const len2 = str2.length;
+const str1 = inputs[0],
+  str2 = inputs[1];
+const len1 = str1.length,
+  len2 = str2.length;
 let answer = 0;
 
-const dp = Array.from(Array(len1 + 1), () => Array(len2 + 1).fill(0));
+const dp = Array(len1).fill(0);
 
-for (let i = 1; i <= len1; i++) {
-  for (let j = 1; j <= len2; j++) {
-    if (str1[i - 1] === str2[j - 1]) {
-      dp[i][j] = dp[i - 1][j - 1] + 1;
-      answer = Math.max(answer, dp[i][j]);
+for (let i = 0; i < len2; i++) {
+  for (let j = len1; j >= 0; j--) {
+    if (str2[i] !== str1[j]) dp[j] = 0;
+    else {
+      if (j === 0) dp[j] = 1;
+      else dp[j] = dp[j - 1] + 1;
+      answer = Math.max(answer, dp[j]);
     }
   }
 }
