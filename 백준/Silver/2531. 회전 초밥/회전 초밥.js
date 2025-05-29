0ba795487b4c27f818de66267c4d576150f39f1a
chichoc <chichoc.dev@gmail.com>
2025-05-30 00:06:15 +0900

    [Silver I] Title: 회전 초밥, Time: 156 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.js
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.js
@@ -1,20 +1,22 @@
 const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const [N, d, k, c] = inputs[0].split(' ').map(Number);
 const dishes = inputs.slice(1).map(Number);
+const eating = Array(d + 1).fill(0);
+let count = 0;
 let answer = 0;
 
-for (let i = 0; i < N; i++) {
-  const dish = new Set(eat(i));
-  dish.add(c);
-  answer = Math.max(dish.size, answer);
+for (let i = 0; i < k; i++) {
+  if (eating[dishes[i]]++ === 0) count++;
 }
+answer = count + (eating[c] ? 0 : 1);
 
-function eat(start) {
-  const result = dishes.slice(start, start + k);
-  if (start + k > N) {
-    result.push(...dishes.slice(0, start + k - N));
-  }
-  return result;
+for (let i = 0; i < N; i++) {
+  const endIdx = (i + k) % N;
+
+  if (--eating[dishes[i]] === 0) count--;
+  if (eating[dishes[endIdx]]++ === 0) count++;
+  
+  answer = Math.max(answer, count + (eating[c] ? 0 : 1));
 }
 
 console.log(answer);
