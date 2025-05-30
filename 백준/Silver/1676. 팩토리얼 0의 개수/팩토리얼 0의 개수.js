ed1cc3c614dd8e157088991ba88ab124d9e880aa
chichoc <chichoc.dev@gmail.com>
2025-05-30 11:45:17 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
@@ -1,9 +1,9 @@
-let N = +require('fs').readFileSync(0).toString().trim(); // 입력 한 줄
+let N = +require('fs').readFileSync(0).toString().trim();
 let count = 0;
 
 while (N >= 5) {
-  count += Math.floor(N / 5);
   N = Math.floor(N / 5);
+  count += N;
 }
 
 console.log(count);
