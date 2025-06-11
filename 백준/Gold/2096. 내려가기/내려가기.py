2b0f41b0e145322d0822a007cdc08b0562f1d974
chichoc <chichoc.dev@gmail.com>
2025-06-11 15:56:47 +0900

    [Gold V] Title: 내려가기, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,17 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-mx1, mx2, mx3 = map(int, input().split())
-mn1, mn2, mn3 = mx1, mx2, mx3
+def solution():
+    n = int(input())
+    mx1, mx2, mx3 = map(int, input().split())
+    mn1, mn2, mn3 = mx1, mx2, mx3
 
-for _ in range(n - 1):
-    a, b, c = map(int, input().split())
-    pmx1, pmx2, pmx3, pmn1, pmn2, pmn3 = mx1, mx2, mx3, mn1, mn2, mn3
-    mx1 = a + max(pmx1, pmx2)
-    mx2 = b + max(pmx1, pmx2, pmx3)
-    mx3 = c + max(pmx2, pmx3)
-    mn1 = a + min(pmn1, pmn2)
-    mn2 = b + min(pmn1, pmn2, pmn3)
-    mn3 = c + min(pmn2, pmn3)
-print(max(mx1, mx2, mx3), min(mn1, mn2, mn3))
+    for _ in range(1, n):
+        nmx1, nmx2, nmx3 = map(int, input().split())
+        nmn1, nmn2, nmn3 = nmx1, nmx2, nmx3
+
+        nmx1 += max(mx1, mx2)
+        nmx2 += max(mx1, mx2, mx3)
+        nmx3 += max(mx2, mx3)
+        mx1, mx2, mx3 = nmx1, nmx2, nmx3
+
+        nmn1 += min(mn1, mn2)
+        nmn2 += min(mn1, mn2, mn3)
+        nmn3 += min(mn2, mn3)
+        mn1, mn2, mn3 = nmn1, nmn2, nmn3
+
+    print(max(mx1, mx2, mx3), min(mn1, mn2, mn3))
+
+if __name__ == "__main__":
+    solution()
