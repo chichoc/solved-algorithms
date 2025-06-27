1e77f7907dc8552ae649aa1ab01cbee4c117b819
chichoc <chichoc.dev@gmail.com>
2025-06-27 21:09:19 +0900

    [Gold III] Title: 후다다닥을 이겨 츄르를 받자!, Time: 2352 ms, Memory: 16616 KB -BaekjoonHub

--- 백준/Gold/23030. 후다다닥을 이겨 츄르를 받자！/후다다닥을 이겨 츄르를 받자！.js
+++ 백준/Gold/23030. 후다다닥을 이겨 츄르를 받자！/후다다닥을 이겨 츄르를 받자！.js
@@ -2,62 +2,50 @@ const inputs = require('fs').readFileSync(0).toString().trim().split('\n');
 const N = +inputs[0];
 const station = inputs[1].split(' ').map(Number);
 const M = +inputs[2];
-const transfer = inputs.slice(3, 3 + M).map((input) => input.split(' ').map(Number));
-const K = +inputs[3 + M];
 const users = inputs.slice(4 + M).map((input) => input.split(' ').map(Number));
 let idx = 0;
-const nodeMap = [];
 
-for (let i = 0; i < N; i++) {
-  nodeMap.push([]);
-  for (let j = 0; j < station[i]; j++) {
-    nodeMap[i][j] = idx++;
-  }
-}
-const totalNodes = idx;
-const graph = Array.from({ length: totalNodes }, () => []);
+const stationsIdx = Array.from({ length: N }, (_, i) => Array.from({ length: station[i] }, () => idx++));
+const stationCount = idx;
+const graph = Array.from({ length: stationCount }, () => []);
 
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < station[i] - 1; j++) {
-    const a = nodeMap[i][j];
-    const b = nodeMap[i][j + 1];
+    const a = stationsIdx[i][j];
+    const b = stationsIdx[i][j + 1];
     graph[a].push([b, 1]);
     graph[b].push([a, 1]);
   }
 }
 
 const transferList = [];
-for (const [a, b, c, d] of transfer) {
-  const n1 = a - 1,
-    s1 = b - 1,
-    n2 = c - 1,
-    s2 = d - 1;
-  transferList.push([nodeMap[n1][s1], nodeMap[n2][s2]]);
+for (const input of inputs.slice(3, 3 + M)) {
+  const [p1, p2, q1, q2] = input.split(' ').map(Number);
+  transferList.push([stationsIdx[p1 - 1][p2 - 1], stationsIdx[q1 - 1][q2 - 1]]);
 }
 
 function dijkstra(start, end, transferCost) {
-  const dist = Array(totalNodes).fill(Infinity);
+  const dist = Array(stationCount).fill(Infinity);
   dist[start] = 0;
   const pq = [[0, start]];
-  
+
   while (pq.length) {
     pq.sort((a, b) => a[0] - b[0]);
     const [cost, node] = pq.shift();
     if (dist[node] < cost) continue;
     for (const [next, weight] of graph[node]) {
-      if (dist[next] > dist[node] + weight) {
-        dist[next] = dist[node] + weight;
-        pq.push([dist[next], next]);
-      }
+      if (dist[next] <= dist[node] + weight) continue;
+      dist[next] = dist[node] + weight;
+      pq.push([dist[next], next]);
     }
 
     for (const [a, b] of transferList) {
-      if (node === a && dist[b] > dist[node] + transferCost) {
-        dist[b] = dist[node] + transferCost;
+      if (node === a && dist[b] > dist[a] + transferCost) {
+        dist[b] = dist[a] + transferCost;
         pq.push([dist[b], b]);
       }
-      if (node === b && dist[a] > dist[node] + transferCost) {
-        dist[a] = dist[node] + transferCost;
+      if (node === b && dist[a] > dist[b] + transferCost) {
+        dist[a] = dist[b] + transferCost;
         pq.push([dist[a], a]);
       }
     }
@@ -66,8 +54,8 @@ function dijkstra(start, end, transferCost) {
 }
 
 for (const [t, u1, u2, v1, v2] of users) {
-  const start = nodeMap[u1 - 1][u2 - 1];
-  const end = nodeMap[v1 - 1][v2 - 1];
+  const start = stationsIdx[u1 - 1][u2 - 1];
+  const end = stationsIdx[v1 - 1][v2 - 1];
   const answer = dijkstra(start, end, t);
   console.log(answer);
 }
