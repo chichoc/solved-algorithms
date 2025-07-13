8d048bc59698faaef87dffbd6feebcbf43f693c4
chichoc <chichoc.dev@gmail.com>
2025-07-13 22:22:08 +0900

    [Gold III] Title: 세 용액, Time: 240 ms, Memory: 11728 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.js
+++ 백준/Gold/2473. 세 용액/세 용액.js
@@ -8,19 +8,23 @@ const answerNums = [];
 let answerSum = Infinity;
 
 for (let i = 0; i < N - 2; i++) {
+  const num1 = nums[i];
   let left = i + 1;
   let right = N - 1;
 
   while (left < right) {
-    const sum = nums[i] + nums[left] + nums[right];
+    const num2 = nums[left], num3 = nums[right];
+    const sum = num1 + num2 + num3;
     if (Math.abs(sum) < Math.abs(answerSum)) {
       answerNums.length = 0;
-      answerNums.push(nums[i], nums[left], nums[right]);
+      answerNums.push(num1, num2, num3);
       answerSum = sum;
     }
-    if (sum < 0) left++;
+    if (sum === 0) break;
+    else if (sum < 0) left++;
     else right--;
   }
+  if (answerSum === 0) break;
 }
 
 console.log(answerNums.join(' '));
