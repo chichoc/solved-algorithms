13b18759cddd1b3bec1482e6af5502421ec46704
chichoc <chichoc.dev@gmail.com>
2025-10-04 00:37:49 +0900

    [level 3] Title: 등산코스 정하기, Time: 260.60 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.js
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.js
@@ -1,23 +1,24 @@
 function solution(n, paths, gates, summits) {
     const graph = Array.from({length: n + 1}, _ => []);
-    paths.forEach(([i, j, w]) => (graph[i].push([j, w]), graph[j].push([i, w])))
+    paths.forEach(([i, j, w]) => (graph[i].push([j, w]), graph[j].push([i, w])));
+    summits.forEach(summit => graph[summit] = []);
     
     let queue = gates;
     const intensities = Array.from({length: n + 1}, _ => 10000001);
     gates.forEach(g => intensities[g] = 0);
     
     while(queue.length > 0) {
-        const nq = [];
+        const nextQueue = [];
         for (const node of queue) {
             for (const [v, w] of graph[node]){
                 const maxV = Math.max(intensities[node], w)
                 if (intensities[v] > maxV){
                     intensities[v] = maxV;
-                    if (!summits.includes(v)) nq.push(v)
+                    nextQueue.push(v)
                 }   
             }
         }
-        queue = [...nq];
+        queue = [...nextQueue];
     }
     
     return summits.map(s => [s, intensities[s]]).sort((a, b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1])[0];
